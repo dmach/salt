1ec0cdc1b9454c841868b9158d047a8827203114
Marcus Rückert <darix@nordisch.org>
2022-05-25 15:13:12 +0200

    Make the pass renderer more configurable
    1. allow us to make the pass renderer fail hard during pillar rendering
       For this we add a master config variable pass_renderer_fail_hard
    
    2. allow to have prefix for variables that should be passed through pass
       For this we add a master config variable pass_variable_prefix
    
       This would lower the number of false positives triggered in the hard
       fail case.
    
    only strip trailing newlines

--- salt/renderers/pass.py
+++ salt/renderers/pass.py
@@ -75,6 +75,17 @@ def _fetch_secret(pass_path):
     Fetch secret from pass based on pass_path. If there is
     any error, return back the original pass_path value
     """
+    # remove the optional path prefix
+    if "pass_variable_prefix" in __opts__:
+        pass_prefix = __opts__["pass_variable_prefix"]
+
+        # if we do not see our prefix we do not want to process this variable
+        if not pass_path.startswith(pass_prefix):
+            return pass_path
+
+        # strip the prefix from the start of the string
+        pass_path = pass_path[len(pass_prefix):]
+
     # Remove whitespaces from the pass_path
     pass_path = pass_path.strip()
 
@@ -92,7 +103,10 @@ def _fetch_secret(pass_path):
     # The version of pass used during development sent output to
     # stdout instead of stderr even though its returncode was non zero.
     if proc.returncode or not pass_data:
-        log.warning("Could not fetch secret: %s %s", pass_data, pass_error)
+        if "pass_renderer_fail_hard" in __opts__ and __opts__["pass_renderer_fail_hard"]:
+            raise SaltRenderError("Unable to retrieve secret '{}' from the password store: {}".format(pass_path, stderr))
+        else:
+            log.warning("Could not fetch secret: %s %s", pass_data, pass_error)
         pass_data = pass_path
     return pass_data.rstrip("\r\n")
 
