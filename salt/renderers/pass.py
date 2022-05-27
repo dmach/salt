09b84e3468e4ac1de82da546c12c132676d18ca3
Daniel Mach <daniel.mach@suse.com>
2022-05-27 16:13:49 +0200

    Make the pass renderer more configurable
    1. Allow us to make the pass renderer fail during pillar rendering
       when a secret corresponding with a pass path cannot be fetched.
       For this we add a master config variable pass_strict_fetch.
    
    2. Allow to have prefix for variables that should be processed
       with the pass renderer.
       For this we add a master config variable pass_variable_prefix.
    
    3. Allow us to configure pass' GNUPGHOME and PASSWORD_STORE_DIR
       environmental variables.
       For this we add master config variables pass_gnupghome and pass_path.

--- salt/renderers/pass.py
+++ salt/renderers/pass.py
@@ -45,6 +45,30 @@ Install pass binary
 
         pass:
           pkg.installed
+
+Configuration options
+
+.. code-block:: yaml
+
+        # If the prefix is set, only the prefixed template variables are
+        # considered for fetching secrets from Pass. The default behavior
+        # is that *all* variables are considered.
+        # When the prefix matching is on, You may also consider
+        # setting the `renderer: jinja|yaml|pass` config option.
+        # recommended value: 'pass:'
+        pass_variable_prefix: 'pass:'
+
+        # If set to 'true', error out when Pass is unable to fetch a secret
+        # for a template variable. Must be used with `pass_variable_prefix`.
+        pass_strict_fetch: true
+
+        # set GNUPGHOME env for Pass
+        # defaults to ~/.gnupg
+        pass_gnupghome: <path>
+
+        # set PASSWORD_STORE_DIR env for Pass
+        # defaults to ~/.password-store
+        pass_path: <path>
 """
 
 
@@ -75,6 +99,23 @@ def _fetch_secret(pass_path):
     Fetch secret from pass based on pass_path. If there is
     any error, return back the original pass_path value
     """
+
+    # Remove the optional prefix from pass path
+    pass_prefix = __opts__.get("pass_variable_prefix", None)
+    if pass_prefix:
+        # If we do not see our prefix we do not want to process this variable
+        if not pass_path.startswith(pass_prefix):
+            return pass_path
+
+        # strip the prefix from the start of the string
+        pass_path = pass_path[len(pass_prefix):]
+
+    # The pass_strict_fetch option must be used with pass_variable_prefix
+    pass_strict_fetch = __opts__.get("pass_strict_fetch", False)
+    if pass_strict_fetch and not pass_prefix:
+        msg = "The 'pass_strict_fetch' option requires 'pass_variable_prefix' option enabled"
+        raise SaltConfigurationError(msg)
+
     # Remove whitespaces from the pass_path
     pass_path = pass_path.strip()
 
@@ -86,12 +127,23 @@ def _fetch_secret(pass_path):
     env = os.environ.copy()
     env["HOME"] = expanduser("~")
 
+    if "pass_dir" in __opts__:
+        env["PASSWORD_STORE_DIR"] = __opts__["pass_dir"]
+
+    if "pass_gnupghome" in __opts__:
+        env["GNUPGHOME"] = __opts__["pass_gnupghome"]
+
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=env)
     pass_data, pass_error = proc.communicate()
 
     # The version of pass used during development sent output to
     # stdout instead of stderr even though its returncode was non zero.
     if proc.returncode or not pass_data:
+        msg = "Could not fetch secret '{}' from the password store: {}".format(pass_path, pass_error)
+        if pass_strict_fetch:
+            raise SaltRenderError(msg)
+        else:
+            log.warning(msg)
         log.warning("Could not fetch secret: %s %s", pass_data, pass_error)
         pass_data = pass_path
     return pass_data.rstrip("\r\n")
